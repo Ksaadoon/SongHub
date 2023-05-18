afc59fdf93ef7d54b4f1c149c38421a514d60610
Phillipi Casado <ksaadoon@gmail.com>
2023-05-17 21:07:48 -0400

    db models connection

---
+++ songhub/songhub/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+#register your models here so we can manage them in admin
+admin.site.register(User)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(FavoriteSong)
