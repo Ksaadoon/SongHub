f662c0d3de5ff85da60c64e5482514e4c9cda028
Ksaadoon <127905113+Ksaadoon@users.noreply.github.com>
2023-05-19 01:58:45 -0400

    Delete songhub/songhub directory

--- songhub/songhub/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-#register your models here so we can manage them in admin
-admin.site.register(User)
-admin.site.register(Artist)
-admin.site.register(Album)
-admin.site.register(Song)
-admin.site.register(FavoriteSong)
